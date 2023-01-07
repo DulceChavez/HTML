500e858e738618b59a13b522a1424071571f96cc
unknown <dulce.chavez@ghimprove.com>
2023-01-07 14:41:51 -0600

    Validacion formulario campos obligatorios

--- miscript.js
+++ miscript.js
@@ -16,34 +16,137 @@ function validacion (){
     const nombre =  document.getElementById('nombre');
     console.log (nombre);
     console.log (nombre.value);
+    const expresionnombre = new RegExp("^.{3,15}$");
     if (nombre.value ==''){   
         document.getElementById('validacionnombre').textContent = 'El campo es requerido';
         nombre.style.border = '1px solid #ff0000';
     }
 
+    else if(!expresionnombre.test(nombre.value)){
+        document.getElementById('validacionnombre').textContent = 'Escribir mínimo 3 caracteres';
+        nombre.style.border = '1px solid #ff0000'; 
+    }
+
+    else{
+        document.getElementById('validacionnombre').textContent = '';
+        nombre.style.border = '1px solid #AAAAAA'; 
+    }
+
     const correo = document.getElementById('correo');
     console.log (correo);
     console.log (correo.value);
-    if (correo.value ==''){   
+    const expresionregular = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$"); 
+    console.log(expresionregular);
+    console.log(expresionregular.test(correo.value));
+    if(correo.value ==''){   
         document.getElementById('validacioncorreo').textContent = '*El campo es requerido';
         correo.style.border = '1px solid #ff0000';
+    }
+    
+    else if (!expresionregular.test(correo.value)){
+        document.getElementById('validacioncorreo').textContent = '*Formato de correo incorrecto';
+        correo.style.border = '1px solid #ff0000'; 
+    }
+    else{
+        document.getElementById('validacioncorreo').textContent = '';
+        correo.style.border = '1px solid #AAAAAA'; 
+    }
+
+    /*--includes--*/
+    /*if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(correo.value)){
+        alert ("Correo valido");
+    }
+    else{
+        alert ("Correo invalido");
+    }
+    /*console.log(!correo.value.includes("@"));
+    console.log(!correo.value.includes("."));
+    console.log((!correo.value.includes("@") || !correo.value.includes(".")));
+    if (correo.value.includes("@") || correo.value.includes(".")){        
+        document.getElementById('validacioncorreo').textContent = '';
+        correo.style.border = '1px solid #AAAAAA';        
+    }
+    else {        
+        document.getElementById('validacioncorreo').textContent = 'Formato de correo incorrecto';
+        correo.style.border = '1px solid #ff0000';
+    }*/
+    
+    const telefono = document.getElementById('telefono');
+    console.log (telefono);
+    console.log (telefono.value);
+    const expresiontelefono = new RegExp("[0-9]{10}");
+    console.log(expresiontelefono);
+    console.log(expresiontelefono.test(telefono.value));
+    if (telefono.value == ''){
+        document.getElementById('validaciontelefono').textContent = '';
+        telefono.style.border = '1px solid #AAAAAA';
+    }
+    else if(!expresiontelefono.test(telefono.value)){
+        document.getElementById('validaciontelefono').textContent = 'Longitud de 10 números';
+        telefono.style.border = '1px solid #ff0000';   
+    } 
+    else{
+        document.getElementById('validaciontelefono').textContent = '';
+        telefono.style.border = '1px solid #AAAAAA';
+    }
+
+    const sitioweb = document.getElementById('sitioweb');
+    console.log (sitioweb);
+    console.log(sitioweb.value);
+    console.log(!sitioweb.value.includes("http://"));
+    console.log(!sitioweb.value.includes("https://"));
+    if (sitioweb.value == ''){
+        document.getElementById('validacionsitioweb').textContent = '';
+        sitioweb.style.border = '1px solid #AAAAAA';
+    }
+    else if(sitioweb.value.includes("https://") || sitioweb.value.includes("http://")){        
+        document.getElementById('validacionsitioweb').textContent = '';
+        sitioweb.style.border = '1px solid #AAAAAA';        
+    }
+    else {        
+        document.getElementById('validacionsitioweb').textContent = 'Comenzar con http:// o https://';
+        sitioweb.style.border = '1px solid #ff0000';
     }
 
     const asunto = document.getElementById('asunto');
     console.log (asunto);
     console.log(asunto.value);
-    if (asunto.value ==''){   
+    const expreg = new RegExp("^.{5,50}$"); 
+    console.log(expreg);
+    console.log(expreg.test(asunto.value));
+    if(asunto.value ==''){   
         document.getElementById('validacionasunto').textContent = '*El campo es requerido';
         asunto.style.border = '1px solid #ff0000';
     }
-
+    
+    else if (!expreg.test(asunto.value)){
+        document.getElementById('validacionasunto').textContent = 'Escribir minimo 5 caracteres';
+        asunto.style.border = '1px solid #ff0000'; 
+    }
+    else{
+        document.getElementById('validacionasunto').textContent = '';
+        asunto.style.border = '1px solid #AAAAAA'; 
+    }
+    
     const mensaje = document.getElementById('mensaje');
     console.log (mensaje);
-    console.log (mensaje.value);
+    console.log (mensaje.value);    
+    const mensajeexpre = new RegExp("^.{10,100}$"); 
+    console.log(mensajeexpre);
+    console.log(mensajeexpre.test(mensaje.value));
     if (mensaje.value ==''){   
         document.getElementById('validacionmensaje').textContent = '*El campo es requerido';
         mensaje.style.border = '1px solid #ff0000';
     }
+    else if(!mensajeexpre.test(mensaje.value)){
+        document.getElementById('validacionmensaje').textContent = 'Escribir mínimo 10 caracteres';
+        mensaje.style.border = '1px solid #ff0000'; 
+    }
+    else{
+        document.getElementById('validacionmensaje').textContent = '';
+        mensaje.style.border = '1px solid #AAAAAA'; 
+    }
+    
 }
 
 const eliminar = () => {
@@ -55,6 +158,14 @@ const eliminar = () => {
     document.getElementById('validacioncorreo').textContent = '';
     correo.style.border = '1px solid #AAAAAA';
 
+    const telefonoeli = document.getElementById('telefono');
+    document.getElementById('validaciontelefono').textContent = '';
+    telefono.style.border = '1px solid #AAAAAA';
+
+    const sitiowebeli = document.getElementById('sitioweb');
+    document.getElementById('validacionsitioweb').textContent = '';
+    sitioweb.style.border = '1px solid #AAAAAA';
+
     const asuntoeli = document.getElementById('asunto');
     document.getElementById('validacionasunto').textContent = '';
     asunto.style.border = '1px solid #AAAAAA';
@@ -63,3 +174,12 @@ const eliminar = () => {
     document.getElementById('validacionmensaje').textContent = '';
     mensaje.style.border = '1px solid #AAAAAA';
 }
+
+const mensaje = document.getElementById('mensaje');
+const contador = document.getElementById('contador');
+mensaje.addEventListener('input', function(e) {
+    const target = e.target;
+    const longitudMax = target.getAttribute('maxlength');
+    const longitudAct = target.value.length;
+    contador.innerHTML = `${longitudAct}/${longitudMax}`;
+});
